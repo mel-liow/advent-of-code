ea3c53a3b9bd8342a89166ad4d4d1e0177c81a5c
mel-liow <mel.liow.94@gmail.com>
2021-12-25 20:52:51 -0800

    complete part2

--- day5_hydrothermal-venture/solution.py
+++ day5_hydrothermal-venture/solution.py
@@ -35,29 +35,50 @@ def part1(data):
         x2, y2 = row[1]
 
         if x1 == x2:
-            step = 1 if y1 < y2 else -1
-            stop = y2+1 if y1 < y2 else y2-1
-            for i in range(y1, stop, step):
+            for i in range(min([y1, y2]), max([y1, y2]) + 1, 1):
                 d[x1][i] += 1
 
         if y1 == y2:
-            step = 1 if x1 < x2 else -1
-            stop = x2+1 if x1 < x2 else x2-1
-            for j in range(x1, stop, step):
+            for j in range(min([x1, x2]), max([x1, x2]) + 1, 1):
                 d[j][y1] += 1
     
     return count_points(d)
-        
+
+def part2(data):
+    
+    d = defaultdict(lambda: defaultdict(int))
+    for row in data:
+        x1, y1 = row[0]
+        x2, y2 = row[1]
+
+        if x1 == x2:
+            for i in range(min([y1, y2]), max([y1, y2]) + 1, 1):
+                d[x1][i] += 1
+
+        elif y1 == y2:
+            for j in range(min([x1, x2]), max([x1, x2]) + 1, 1):
+                d[j][y1] += 1
+
+        elif abs((y2-y1) / (x2-x1)) == 1:
+            step_y = 1 if y1 < y2 else -1
+            step_x = 1 if x1 < x2 else -1
+
+            stop_x = x2+1 if x1 < x2 else x2-1
+            for k in range(x1, stop_x, step_x):
+                d[k][y1] += 1
+                y1 = y1 + step_y
+
+    return count_points(d)
 
 def main(path):
     raw_data = read_data(path)
 
     coordinates = parse_data(raw_data)
     solution1 = part1(coordinates)
-    # solution2 = part2(random_numbers, boards)
+    solution2 = part2(coordinates)
 
     print(f'solution1: ', solution1),
-    # print(f'solution2: ', solution2)
+    print(f'solution2: ', solution2)
     
 
 if __name__ == "__main__":
